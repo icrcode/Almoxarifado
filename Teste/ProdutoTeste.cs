db8d5123a02a500522620ed8142d0cb27419b947
Ícaro <icarobotelhosocial@gmail.com>
2023-08-23 21:23:27 -0300

    Adicionar arquivos de projeto.

---
+++ Teste/ProdutoTeste.cs
@@ -0,0 +1,24 @@
+namespace Teste
+{
+    public class ProdutoTeste
+    {
+
+        /*
+         Eu como almoxarife preciso cadastrar produtos para que sejam 
+        consumidos pelos funcionarios da empresa
+
+        Critérios de aceite:
+        - Um novo produto deve ter obrigatoriamente um codigo único, nome, saldo 0, custo 0, unidade de medida.
+        - As unidades de medidas são: PC, KG, MT, GR, LT, CX.
+        - Um produto PODE ter uma descrição.
+        - Um produto pode ter um codigo de barras.
+
+         */
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
